79aa1b39961e69a86883699c7bfe2dcaeb10953f
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-08-30 13:09:04 +0900

    [D3] Title: 부분 수열의 합, Time: 1,404 ms, Memory: 23,804 KB -BaekjoonHub

---
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Solution {
+	static int target;
+	static int cnt = 0;
+	static int ans=0;
+	static boolean[] visited;
+	static int[] nums;
+
+	static void powerset(int index) {
+		if (index == visited.length) {
+			for (int i = 0; i < visited.length; i++) {
+				if (visited[i]) {
+					cnt += nums[i];
+				}
+			}
+			if (cnt == target) {
+				ans++;
+			}
+			cnt = 0;
+
+			return;
+		}
+
+		visited[index] = true;
+		powerset(index + 1);
+
+		visited[index] = false;
+		powerset(index + 1);
+
+		return;
+	}
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int testCases = scanner.nextInt();
+
+		for (int tc = 1; tc <= testCases; tc++) {
+
+			int n = scanner.nextInt();
+			target = scanner.nextInt();
+			nums = new int[n];
+			visited = new boolean[n];
+
+			for (int i = 0; i < n; i++) {
+				nums[i] = scanner.nextInt();
+			}
+			
+			powerset(0);
+			System.out.printf("#%d %d\n",tc,ans);
+			ans = 0;
+			
+
+		}
+
+		scanner.close();
+	}
+}
