e7bfb44825efe10648dff198b6972ec8180a2c29
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-09-13 22:53:09 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 80.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        int remain = n;
+        while(remain>=a) {
+            answer += (remain/a)*b;
+            remain = remain%a+(remain/a)*b;
+        }
+
+        
+        
+        return answer;
+    }
+}
