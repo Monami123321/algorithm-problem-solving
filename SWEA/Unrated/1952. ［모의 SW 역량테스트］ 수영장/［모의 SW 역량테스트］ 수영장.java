f6b2b841f2e94162ce96134bddf45244d4106e0f
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-09-19 13:29:28 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 178 ms, Memory: 22,928 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -2,47 +2,42 @@ import java.util.Scanner;
 
 public class Solution {
 	static int ans;
-	static int[] arr;
-	
-	static void dfs(int depth, int sum, int dayCharge, int monthCharge, int quarterCharge) {
-		if(depth>=12) {
+
+	static void dfs(int depth, int dayCharge, int monthCharge, int quarterCharge, int[] plan, int sum) {
+		if (depth > 11) {
 			ans = Math.min(ans, sum);
 			return;
-			
 		}
-		
-		dfs(depth+1, sum+arr[depth]*dayCharge, dayCharge, monthCharge, quarterCharge);
-		dfs(depth+1, sum+monthCharge, dayCharge, monthCharge, quarterCharge);
-		dfs(depth+3, sum+quarterCharge, dayCharge, monthCharge, quarterCharge);
-		
+
+		dfs(depth + 1, dayCharge, monthCharge, quarterCharge, plan, sum + dayCharge * plan[depth]);
+		dfs(depth + 1, dayCharge, monthCharge, quarterCharge, plan, sum + monthCharge);
+		dfs(depth + 3, dayCharge, monthCharge, quarterCharge, plan, sum + quarterCharge);
+
 	}
-	
-	
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		int testCases = scanner.nextInt();
+
 		for (int tc = 1; tc <= testCases; tc++) {
 			int dayCharge = scanner.nextInt();
 			int monthCharge = scanner.nextInt();
 			int quarterCharge = scanner.nextInt();
-			//int yearCharge = scanner.nextInt();
-			ans = scanner.nextInt();
+			ans = scanner.nextInt(); // 1년 이용권
 
-			arr = new int[12];
-			for (int i = 0; i < arr.length; i++) {
-				arr[i] = scanner.nextInt();
+			int[] plan = new int[12];
+			for (int i = 0; i < plan.length; i++) {
+				plan[i] = scanner.nextInt();
 
 			}
-			
-			dfs(0, 0, dayCharge, monthCharge, quarterCharge);
-			System.out.printf("#%d %d\n",tc,ans);
-			
-			
-			
+			dfs(0, dayCharge, monthCharge, quarterCharge, plan, 0);
+			System.out.printf("#%d %d\n", tc, ans);
+
 		}
 
 		scanner.close();
+
 	}
 
 }
