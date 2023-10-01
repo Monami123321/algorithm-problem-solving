da60c85bd54ab3c44ebcfa75a1c2a988b545bf9b
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-10-01 23:29:42 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 163 ms, Memory: 24,892 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,43 +1,38 @@
 import java.util.Scanner;
 
 public class Solution {
-	static int ans;
-
-	static void dfs(int depth, int dayCharge, int monthCharge, int quarterCharge, int[] plan, int sum) {
-		if (depth > 11) {
-			ans = Math.min(ans, sum);
-			return;
-		}
-
-		dfs(depth + 1, dayCharge, monthCharge, quarterCharge, plan, sum + dayCharge * plan[depth]);
-		dfs(depth + 1, dayCharge, monthCharge, quarterCharge, plan, sum + monthCharge);
-		dfs(depth + 3, dayCharge, monthCharge, quarterCharge, plan, sum + quarterCharge);
-
-	}
-
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		int testCases = scanner.nextInt();
-
 		for (int tc = 1; tc <= testCases; tc++) {
-			int dayCharge = scanner.nextInt();
-			int monthCharge = scanner.nextInt();
-			int quarterCharge = scanner.nextInt();
-			ans = scanner.nextInt(); // 1년 이용권
+			int daycharge = scanner.nextInt();
+			int monthcharge = scanner.nextInt();
+			int quartercharger = scanner.nextInt();
+			int ans = scanner.nextInt();
 
-			int[] plan = new int[12];
-			for (int i = 0; i < plan.length; i++) {
+			int[] dp = new int[13];
+			int[] plan = new int[13];
+			for (int i = 1; i < plan.length; i++) {
 				plan[i] = scanner.nextInt();
 
 			}
-			dfs(0, dayCharge, monthCharge, quarterCharge, plan, 0);
-			System.out.printf("#%d %d\n", tc, ans);
+
+			for (int i = 1; i < dp.length; i++) {
+				if (i < 3) {
+					dp[i] = dp[i - 1] + Math.min(daycharge * plan[i], monthcharge);
+				} else {
+					dp[i] = Math.min(dp[i - 1] + Math.min(daycharge * plan[i], monthcharge),
+							dp[i - 3] + quartercharger);
+				}
+
+			}
+			ans = Math.min(ans, dp[12]);
+			System.out.printf("#%d %d\n",tc,ans);
 
 		}
 
 		scanner.close();
-
 	}
 
 }
