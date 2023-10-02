7431a0f9a48a78392dc56394bbd392438a5e4ce1
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-10-02 21:47:53 +0900

    [Silver III] Title: 1로 만들기, Time: 116 ms, Memory: 16352 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+
+		int[] dp = new int[n + 1];
+		for (int i = 2; i < dp.length; i++) {
+			if (i % 6 == 0) {
+				if (dp[i / 2] + 1 > dp[i / 3] + 1) {
+					dp[i] = Math.min(dp[i / 3] + 1, dp[i - 1] + 1);
+				} else {
+					dp[i] = Math.min(dp[i / 2] + 1, dp[i - 1] + 1);
+				}
+			} else if (i % 3 == 0) {
+				dp[i] = Math.min(dp[i - 1] + 1, dp[i / 3] + 1);
+			} else if (i % 2 == 0) {
+				dp[i] = Math.min(dp[i - 1] + 1, dp[i / 2] + 1);
+
+			} else {
+				dp[i] = dp[i - 1] + 1;
+			}
+
+		}
+
+		System.out.println(dp[n]);
+
+		br.close();
+	}
+
+}
