cf9f98e0e3287d3f9682fd70c21d5a30b73684ac
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-12-11 23:38:39 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 보급로, Time: 230 ms, Memory: 34,176 KB -BaekjoonHub

--- SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
+++ SWEA/D4/1249. ［S／W 문제해결 응용］ 4일차 － 보급로/［S／W 문제해결 응용］ 4일차 － 보급로.java
@@ -1,68 +1,66 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int[] dr = { -1, 1, 0, 0 };
-	static int[] dc = { 0, 0, -1, 1 };
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    static int[] dr = {-1, 1, 0, 0};
+    static int[] dc = {0, 0, -1, 1};
 
-		int testCases = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        int testCases = Integer.parseInt(br.readLine());
 
-		for (int tc = 1; tc <= testCases; tc++) {
-			int n = Integer.parseInt(br.readLine());
+        tc:
+        for (int i = 1; i <= testCases; i++) {
+            int n = Integer.parseInt(br.readLine());
 
-			int[][] map = new int[n][n];
-			for (int i = 0; i < n; i++) {
-				st = new StringTokenizer(br.readLine(), "0123456789", true);
-				for (int j = 0; j < n; j++) {
-					map[i][j] = Integer.parseInt(st.nextToken());
-				}
+            int[][] map = new int[n][n];
+            for (int j = 0; j < n; j++) {
+                st = new StringTokenizer(br.readLine(), "0123456789", true);
+                for (int k = 0; k < n; k++) {
+                    map[j][k] = Integer.parseInt(st.nextToken());
+                }
+            }
+            PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+            boolean[][] visited = new boolean[n][n];
 
-			}
-			int[][] acc = new int[n][n];
-			for (int i = 0; i < acc.length; i++) {
-				for (int j = 0; j < acc.length; j++) {
-					acc[i][j] = Integer.MAX_VALUE;
+            pq.add(new int[]{0, 0, map[0][0]});
 
-				}
+            while (!pq.isEmpty()) {
+                int[] now = pq.poll();
+                int r = now[0];
+                int c = now[1];
+                int accCost = now[2];
+                if (r == n - 1 && c == n - 1) {
+                    sb.append("#").append(i).append(" ").append(accCost).append("\n");
+                    continue tc;
+                }
+                if(visited[r][c])
+                    continue ;
+                visited[r][c] = true;
 
-			}
-			acc[0][0] = 0;
+                for (int j = 0; j < 4; j++) {
+                    int nr = r + dr[j];
+                    int nc = c + dc[j];
+                    if (nr < 0 || nr > n - 1 || nc < 0 || nc > n - 1 || visited[nr][nc]) {
+                        continue;
+                    }
 
-			Queue<int[]> queue = new LinkedList<>();
-			queue.add(new int[] { 0, 0 });
+                    pq.add(new int[]{nr, nc, accCost + map[nr][nc]});
 
-			while (!queue.isEmpty()) {
-				int[] node = queue.poll();
-				int r = node[0];
-				int c = node[1];
 
-				for (int i = 0; i < 4; i++) {
-					int nr = r + dr[i];
-					int nc = c + dc[i];
-					if (nr < 0 || nr > n - 1 || nc < 0 || nc > n - 1 || acc[r][c] + map[nr][nc] >= acc[nr][nc]) {
-						continue;
-					}
-					
-					acc[nr][nc] = acc[r][c]+map[nr][nc]; 
-					queue.add(new int[] {nr,nc});
+                }
 
-				}
+            }
 
-			}
-			
-			System.out.printf("#%d %d\n",tc,acc[n-1][n-1]);
 
-		}
+        }
+        System.out.print(sb);
 
-		br.close();
-	}
+    }
 
 }
