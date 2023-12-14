9493a62d73183955128b832846626238aee022af
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-12-15 07:38:06 +0900

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(void) {
+
+    int x, n;
+    int total = 0;
+    scanf("%d", &x);
+    scanf("%d", &n);
+    for(int i = 0; i<n; i++) {
+        int a,b;
+        scanf("%d %d", &a, &b);
+        total += (a * b);
+    
+    }
+    if(total == x) {
+        printf("Yes");
+    
+    } else {
+        printf("No");
+    }
+    
+    return 0;
+
+}
