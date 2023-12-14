0d13834ad6557170cdb51c6b03a07da754a1e1a5
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-12-15 08:45:46 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{   
+    int a, b;
+    while(scanf("%d",&a) != EOF) {
+        scanf("%d", &b);
+        printf("%d\n",a+b);
+
+
+
+    }
+    return 0;
+}
