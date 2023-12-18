4641d3967f8a994aac476d2436dbe6db33b8a570
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-12-18 19:26:58 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1420 ms, Memory: 328012 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
-import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -18,21 +17,20 @@ public class Main {
         int k = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[n][m];
-
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine(), "01", true);
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][][] visited = new boolean[k + 1][n][m];
 
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[2] - b[2]);
-        boolean[][][] visited = new boolean[k+1][n][m];
+        queue.add(new int[]{0, 0, 1, 0});
+        visited[0][0][0] = true;
         int ans = -1;
-
-        pq.add(new int[]{0, 0, 1, 0});
-        while (!pq.isEmpty()) {
-            int[] now = pq.poll();
+        while (!queue.isEmpty()) {
+            int[] now = queue.poll();
             int r = now[0];
             int c = now[1];
             int cost = now[2];
@@ -40,35 +38,26 @@ public class Main {
             if (r == n - 1 && c == m - 1) {
                 ans = cost;
                 break;
-
             }
-            if (visited[wall][r][c] || wall > k) {
-                continue;
-            }
-            visited[wall][r][c] = true;
-
 
             for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-
                 if (nr < 0 || nr > n - 1 || nc < 0 || nc > m - 1) {
                     continue;
                 }
                 int nextWall = wall;
-
-                if (map[nr][nc] == 1) {
-                    nextWall += 1;
+                if (map[nr][nc] != 0) {
+                    nextWall++;
                 }
                 if (nextWall > k || visited[nextWall][nr][nc]) {
                     continue;
                 }
-                pq.add(new int[]{nr, nc, cost + 1, nextWall});
+                queue.add(new int[]{nr, nc, cost + 1, nextWall});
+                visited[nextWall][nr][nc] = true;
             }
 
-
         }
-
         System.out.println(ans);
 
 
