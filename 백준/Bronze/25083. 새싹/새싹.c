5576e306994edd7fa68fb07e1918376302be9b49
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-12-23 01:14:00 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    char tmp[] = "         ,r\'\"7\nr`-_   ,\'  ,/\n \\. \". L_r\'\n   `~\\/\n      |\n      |\n";
+    printf("%s",tmp);
+    return 0;
+    
+}
