17f2b8b0feeab9df61b2b2fa54ffd515770cab7a
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2023-12-31 02:32:01 +0900

    [Bronze II] Title: 수 정렬하기, Time: 88 ms, Memory: 11828 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -2,28 +2,58 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
+    static void quickSort(int[] arr) {
+        sort(arr, 0, arr.length - 1);
+    }
+
+    private static void sort(int[] arr, int start, int end) {
+        int pivot = partition(arr, start, end);
+        if (pivot < end) {
+            sort(arr, pivot, end);
+        }
+        if (pivot > start + 1) {
+            sort(arr,start,pivot-1);
+        }
+
+    }
+
+    private static int partition(int[] arr, int start, int end) {
+        int pivot = arr[(start + end) / 2];
+        while (end >= start) {
+            while (arr[start] < pivot) {
+                start++;
+            }
+            while (arr[end] > pivot) {
+                end--;
+            }
+            if (end >= start) {
+                int tmp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = tmp;
+                start++;
+                end--;
+            }
+        }
+        return start;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    int tmp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = tmp;
-                }
-            }
-        }
 
+        quickSort(arr);
         for (int i = 0; i < n; i++) {
             sb.append(arr[i]).append("\n");
+
         }
         System.out.print(sb);
+
         br.close();
     }
 }
