5729a517c40272eac14c4cb52d1812d6cfe6ab1c
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-01-02 23:18:28 +0900

    [Gold IV] Title: 단어 수학, Time: 76 ms, Memory: 11552 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,68 +1,34 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.Arrays;
 
 public class Main {
-    static char[][] words;
-    static HashMap<Character, Integer> map;
-    static int size, ans;
-
-    static int calDigit(char[] arr) {
-        int res = map.get(arr[0]);
-        for (int i = 1; i < arr.length; i++) {
-            res *= 10;
-            res += map.get(arr[i]);
-        }
-        return res;
-    }
-
-    static void permutation(Character[] arr, int depth) {
-        if (depth == arr.length) {
-            for (int i = 0; i < arr.length; i++) {
-                map.put(arr[i], 9 - i);
-            }
-            int sum = 0;
-            for (int i = 0; i < words.length; i++) {
-                sum += calDigit(words[i]);
-            }
-            ans = Math.max(ans, sum);
-        }
-
-
-        for (int i = depth; i < arr.length; i++) {
-            swap(arr, i, depth);
-            permutation(arr, depth + 1);
-            swap(arr, i, depth);
-        }
-
-    }
-
-    static void swap(Character[] arr, int a, int b) {
-        char tmp = arr[a];
-        arr[a] = arr[b];
-        arr[b] = tmp;
-    }
-
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        words = new char[n][];
-        map = new HashMap<>();
-        HashSet<Character> set = new HashSet<>();
+        int[] arr = new int[26];
+
+
         for (int i = 0; i < n; i++) {
-            words[i] = br.readLine().toCharArray();
-            for (int j = 0; j < words[i].length; j++) {
-                set.add(words[i][j]);
+            String now = br.readLine();
+            int len = now.length();
+            for (int j = 0; j < len; j++) {
+                arr[now.charAt(j) - 'A'] += (int) Math.pow(10, len - j - 1);
             }
         }
-        size = set.size();
-        Character[] arr = set.toArray(new Character[size]);
-        ans = 0;
-        permutation(arr, 0);
+        Arrays.sort(arr);
+        int num = 9;
+        int ans = 0;
+        for (int i = 25; i >= 0; i--) {
+            if (arr[i] == 0) {
+                break;
+            }
+            ans += arr[i] * num--;
+        }
         System.out.println(ans);
 
 
         br.close();
+
     }
 }
