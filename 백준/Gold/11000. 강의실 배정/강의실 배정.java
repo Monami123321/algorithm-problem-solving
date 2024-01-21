526869072916372f8536bd2a6ac6e87135413270
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-01-21 21:12:37 +0900

    [Gold V] Title: 강의실 배정, Time: 732 ms, Memory: 76152 KB -BaekjoonHub

--- 백준/Gold/11000. 강의실 배정/강의실 배정.java
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -1,43 +1,39 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        int[][] plan = new int[n][2];
+        int[][] lecture = new int[n][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            plan[i][0] = start;
-            plan[i][1] = end;
+            lecture[i][0] = start;
+            lecture[i][1] = end;
         }
-        Arrays.sort(plan, (a, b) -> {
-            if (a[0] != b[0]) {
-                return a[0] - b[0];
+        Arrays.sort(lecture, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
             }
-            return a[1] - b[1];
+            return a[0] - b[0];
         });
-        int ans = 1;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        pq.add(plan[0][1]);
-
+        pq.add(lecture[0][1]);
         for (int i = 1; i < n; i++) {
-            if (plan[i][0] >= pq.peek()) {
+            if (lecture[i][0] >= pq.peek()) {
                 pq.poll();
-                pq.add(plan[i][1]);
+                pq.add(lecture[i][1]);
             } else {
-                ans++;
-                pq.add(plan[i][1]);
+                pq.add(lecture[i][1]);
             }
         }
-        System.out.println(ans);
+        System.out.println(pq.size());
         br.close();
     }
+
 }
