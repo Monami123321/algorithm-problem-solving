c01a44ef494d7007e94e01b59f82abed429fa4db
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-03-02 23:54:36 +0900

    [Silver II] Title: 체인, Time: 800 ms, Memory: 75060 KB -BaekjoonHub

---
+++ 백준/Silver/2785. 체인/체인.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int ans = 0;
+        int index = n - 1;
+        outer:
+        for (int i = 0; i < n; i++) {
+            if (i == index) {
+                break;
+            }
+            int now =
+                    arr[i];
+            while (now > 0) {
+                now--;
+                index--;
+                ans++;
+                if (index == i) {
+                    break outer;
+                }
+            }
+        }
+        System.out.println(ans);
+
+
+        br.close();
+    }
+}
