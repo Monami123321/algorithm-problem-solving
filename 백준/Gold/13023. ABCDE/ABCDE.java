dc7f8e6fef2b9f712f5efeb7c965cc643675be04
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-03-03 18:56:32 +0900

    [Gold V] Title: ABCDE, Time: 420 ms, Memory: 116860 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static boolean flag;
+    static List<Integer>[] adjList;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        adjList = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adjList[a].add(b);
+            adjList[b].add(a);
+        }
+        flag = false;
+        for (int i = 0; i < N; i++) {
+            if (flag) {
+                break;
+            }
+            dfs(i, new boolean[N], 1);
+        }
+        if (flag) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+        br.close();
+    }
+
+    static void dfs(int node, boolean[] visited, int cnt) {
+        if (cnt == 5) {
+            flag = true;
+            return;
+        }
+        if (flag) {
+            return;
+        }
+        visited[node] = true;
+        adjList[node].forEach(e -> {
+            if (visited[e]) {
+                return;
+            }
+            dfs(e, visited, cnt + 1);
+        });
+        visited[node] = false;
+    }
+}
