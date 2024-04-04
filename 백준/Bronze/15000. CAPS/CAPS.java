7546934d80a75268de2d967b5fea9bd858778c71
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-04-04 21:44:35 +0900

    [Bronze V] Title: CAPS, Time: 204 ms, Memory: 21760 KB -BaekjoonHub

--- 백준/Bronze/15000. CAPS/CAPS.java
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -5,7 +5,11 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println(br.readLine().toUpperCase());
+        char[] arr = br.readLine().toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] ^= 32;
+        }
+        System.out.println(arr);
         br.close();
     }
 }
