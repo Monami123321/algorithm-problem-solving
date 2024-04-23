2bfc9f64f22ca61f22c76c6b1b22c9066982c158
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-04-23 22:55:22 +0900

    [Bronze I] Title: 진법 변환 2, Time: 200 ms, Memory: 9596 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.js
@@ -0,0 +1,7 @@
+const [n, r] = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "./input.txt")
+  .toString()
+  .trim()
+  .split(" ")
+  .map(Number);
+console.log(n.toString(r).toUpperCase());
