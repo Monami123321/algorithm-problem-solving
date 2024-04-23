a8b4f5f4bf9c7e5992956c9b842ebc4dbd9db033
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-04-23 13:20:57 +0900

    [Gold IV] Title: 거짓말, Time: 80 ms, Memory: 11584 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.java
+++ 백준/Gold/1043. 거짓말/거짓말.java
@@ -1,61 +1,82 @@
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.BufferedReader;
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+	static int[] parent;
+	static int ref;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		parent = new int[n + 1];
+		for (int i = 1; i < n+1; i++) {
+			parent[i] = i;
+		}
+		st = new StringTokenizer(br.readLine());
+		int len = Integer.parseInt(st.nextToken());
+		int root;
+		if (len > 0) {
+			root = Integer.parseInt(st.nextToken());
+		} else {
+			root = 0;
+		}
+		for (int i = 1; i < len; i++) {
+			union(root, Integer.parseInt(st.nextToken()));
+		}
+		ref = findSet(root);
+		int[][] arr = new int[m][];
 
-        boolean[] visited = new boolean[m];
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> nextSet = new HashSet<>();
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			len = Integer.parseInt(st.nextToken());
+			int[] party = new int[len];
+			party[0] = Integer.parseInt(st.nextToken());
+			int now = findSet(party[0]);
+			for (int j = 1; j < len; j++) {
+				party[j] = Integer.parseInt(st.nextToken());
+				union(now, party[j]);
+			}
+			arr[i] = party;
+		}
+		int ans = 0;
+		outer:for (int i = 0; i < m; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				if (findSet(arr[i][j]) == ref) {
+					continue outer;
+				}
+			}
+			ans++;
+		}
+		System.out.println(ans);
+		br.close();
+	}
 
-        st = new StringTokenizer(br.readLine());
-        int person = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < person; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
-        }
-        List<Integer>[] parties = new ArrayList[m];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int num = Integer.parseInt(st.nextToken());
-            parties[i] = new ArrayList<>();
-            for (int j = 0; j < num; j++) {
-                parties[i].add(Integer.parseInt(st.nextToken()));
-            }
-        }
-        while (!set.isEmpty()) {
-            for (int i = 0; i < m; i++) {
-                if (visited[i]) {
-                    continue;
-                }
-                for (int p : parties[i]) {
-                    if (set.contains(p)) {
-                        visited[i] = true;
-                        break;
-                    }
-                }
-                if (visited[i]) {
-                    for (int p : parties[i]) {
-                        nextSet.add(p);
-                    }
-                }
-            }
-            nextSet.removeAll(set);
-            set = nextSet;
-            nextSet = new HashSet<>();
-        }
-        int ans = 0;
-        for (int i = 0; i < m; i++) {
-            if (!visited[i]) {
-                ans++;
-            }
-        }
-        System.out.println(ans);
-        br.close();
-    }
+	static int findSet(int a) {
+		if (parent[a] != a) {
+			return parent[a] = findSet(parent[a]);
+		}
+		return parent[a];
+	}
+
+	static void union(int a, int b) {
+		int parentA = findSet(a);
+		int parentB = findSet(b);
+		if (parentA == parentB) {
+			return;
+		}
+		if (parentA == ref || parentB == ref) {
+			parent[parentA] = parent[parentB] = ref;
+			return;
+		}
+		if (parentA < parentB) {
+			parent[parentB] = parentA;
+		} else {
+			parent[parentA] = parentB;
+		}
+	}
 }
