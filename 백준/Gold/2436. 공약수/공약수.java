8865c6cc80f4cb7ba3f98c7a23ad36d8edb53c08
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-04-24 12:44:55 +0900

    [Gold V] Title: 공약수, Time: 108 ms, Memory: 12248 KB -BaekjoonHub

---
+++ 백준/Gold/2436. 공약수/공약수.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int GCD = Integer.parseInt(st.nextToken());
+		int LCM = Integer.parseInt(st.nextToken());
+		long target = (long)LCM * GCD;
+		for (int i = (int)Math.sqrt(target); i >= 1; --i) {
+			if (target % i == 0) {
+				int tmp = (int)(target / i);
+				if (getGCD(tmp, i) == GCD && getLCM(tmp, i) == LCM) {
+					System.out.printf("%d %d", i, tmp);
+					return;
+				}
+			}
+		}
+		br.close();
+	}
+	static int getGCD(int a, int b) {
+		while (b > 0) {
+			int tmp = a % b;
+			a = b;
+			b = tmp;
+		}
+		return a;
+	}
+	static long getLCM(int a, int b) {
+		return (long)a * b / getGCD(a, b);
+	}
+}
