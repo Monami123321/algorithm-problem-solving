ac310b2abc95e3f15cbe12e24608676fc5c45599
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-05-06 20:59:23 +0900

    [Gold III] Title: LCA, Time: 576 ms, Memory: 52780 KB -BaekjoonHub

--- 백준/Gold/11437. LCA/LCA.java
+++ 백준/Gold/11437. LCA/LCA.java
@@ -1,32 +1,36 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
     static class Node {
-        Node parent;
-        List<Node> child;
-        int depth, id;
-
+        int parent, id, depth;
+        List<Integer> children;
         public Node(int id) {
             this.id = id;
-            this.child = new ArrayList<>();
+            this.children = new ArrayList<>();
         }
     }
-
-    public static void main(String[] args) throws Exception {
+    static int[][] sparseTable;
+    static int tableSize;
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        Node[] nodes = new Node[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            nodes[i] = new Node(i);
-
+        List<Integer>[] adjList = new ArrayList[n + 1];
+        int tmp = 1;
+        tableSize = 1;
+        while (tmp <= n) {
+            tmp <<= 1;
+            tableSize++;
         }
-        ArrayList<Integer>[] adjList = new ArrayList[n + 1];
-        for (int i = 1; i < n + 1; i++) {
+        sparseTable = new int[n + 1][tableSize];
+        Arrays.fill(sparseTable[1], 1);
+        Node[] nodes = new Node[n + 1];
+        for (int i = 1; i < n+1; i++) {
             adjList[i] = new ArrayList<>();
+            nodes[i] = new Node(i);
         }
         for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
@@ -35,56 +39,66 @@ public class Main {
             adjList[a].add(b);
             adjList[b].add(a);
         }
-
         Queue<Integer> queue = new LinkedList<>();
         boolean[] visited = new boolean[n + 1];
-        queue.add(1);
         visited[1] = true;
+        queue.add(1);
         while (!queue.isEmpty()) {
             int now = queue.poll();
-
             adjList[now].forEach(e -> {
                 if (visited[e]) {
                     return;
                 }
                 queue.add(e);
                 visited[e] = true;
-                nodes[e].parent = nodes[now];
                 nodes[e].depth = nodes[now].depth + 1;
-                nodes[now].child.add(nodes[e]);
-                return;
+                nodes[e].parent = now;
+                nodes[now].children.add(e);
+                sparseTable[e][0] = now;
+                for (int i = 1; i < tableSize; i++) {
+                    sparseTable[e][i] = sparseTable[sparseTable[e][i - 1]][i - 1];
+                }
             });
         }
-
-        int m = Integer.parseInt(br.readLine());
-        for (int i = 0; i < m; i++) {
+        int q = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < q; i++) {
             st = new StringTokenizer(br.readLine());
             Node a = nodes[Integer.parseInt(st.nextToken())];
             Node b = nodes[Integer.parseInt(st.nextToken())];
-
-            if (a.depth > b.depth) {
-                while (a.depth > b.depth) {
-                    a = a.parent;
-                }
-            } else if (b.depth > a.depth) {
-                while (b.depth > a.depth) {
-                    b = b.parent;
+            if (a.depth < b.depth) {
+                Node temp = a;
+                a = b;
+                b = temp;
+            }
+            int diff = a.depth - b.depth;
+            int index = 0;
+            while (diff > 0) {
+                if ((diff & 1 << index) != 0) {
+                    a = nodes[sparseTable[a.id][index]];
+                    diff ^= 1 << index++;
+                } else {
+                    index++;
                 }
             }
-
             if (a == b) {
                 sb.append(a.id).append("\n");
-                continue;
             } else {
-                while (a.parent != b.parent) {
-                    a = a.parent;
-                    b = b.parent;
+                index = tableSize;
+                while (index-- > 0) {
+                    if(sparseTable[a.id][index] != sparseTable[b.id][index]) {
+                        a = nodes[sparseTable[a.id][index]];
+                        b = nodes[sparseTable[b.id][index]];
+                    }
                 }
-                sb.append(a.parent.id).append("\n");
+                sb.append(a.parent).append("\n");
             }
         }
         System.out.print(sb);
 
 
+
+        br.close();
     }
 }
+
