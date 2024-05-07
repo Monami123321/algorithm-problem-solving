7ac2353cb9587a161e1938258de2858c56fd7221
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-05-07 21:16:01 +0900

    [Silver IV] Title: 동전 0, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int index = n - 1;
+        int cnt = 0;
+        while (k > 0) {
+            cnt += k / arr[index];
+            k %= arr[index--];
+        }
+        System.out.println(cnt);
+        br.close();
+    }
+}
