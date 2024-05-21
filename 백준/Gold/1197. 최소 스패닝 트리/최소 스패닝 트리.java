0d50b6bba643b7b3cce71916bf4181108b9e67aa
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-05-21 20:43:28 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 728 ms, Memory: 59708 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,86 +1,47 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
 public class Main {
-	static class Edge implements Comparable<Edge> {
-		int a, b;
-		long w;
-
-		public Edge(int a, int b, long w) {
-			super();
-			this.a = a;
-			this.b = b;
-			this.w = w;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-
-			return Long.compare(this.w, o.w);
-		}
-
-	}
-
-	static int V, E;
-	static int[] parent;
-
-	static int findset(int x) {
-		if (x != parent[x]) {
-			parent[x] = findset(parent[x]);
-
-		}
-		return parent[x];
-
-	}
-
-	static void union(int x, int y) {
-		parent[findset(y)] = findset(x);
-
-	}
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		V = scanner.nextInt();
-		E = scanner.nextInt();
-
-		Edge[] edges = new Edge[E];
-		parent = new int[V + 1];
-		for (int i = 1; i < V + 1; i++) {
-			parent[i] = i;
-
-		}
-
-		for (int i = 0; i < E; i++) {
-			int a = scanner.nextInt();
-			int b = scanner.nextInt();
-			long c = scanner.nextInt();
-
-			edges[i] = new Edge(a, b, c);
-
-		}
-
-		Arrays.sort(edges);
-		int pick = 0;
-		long ans = 0L;
-		for (int i = 0; i < E; i++) {
-			Edge edge = edges[i];
-
-			if (findset(edges[i].a) == findset(edges[i].b)) {
-				continue;
-			}
-
-			pick++;
-			ans += edge.w;
-			union(edge.a, edge.b);
-			if (pick == V - 1) {
-				break;
-			}
-
-		}
-		System.out.println(ans);
-
-		scanner.close();
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        List<int[]>[] adjList = new ArrayList[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            adjList[a].add(new int[]{b, c});
+            adjList[b].add(new int[]{a, c});
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        boolean[] visited = new boolean[n + 1];
+        pq.add(new int[]{1, 0});
+        int ans = 0;
+        int pick = 0;
+        while (pick < n || !pq.isEmpty()) {
+            int[] now = pq.poll();
+            if (visited[now[0]]) {
+                continue;
+            }
+            visited[now[0]] = true;
+            pick++;
+            ans += now[1];
+            adjList[now[0]].forEach(e -> {
+                if (visited[e[0]]) {
+                    return;
+                }
+                pq.add(e);
+            });
+        }
+        System.out.println(ans);
+        br.close();
+    }
 }
