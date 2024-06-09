3cae11a0fedf6eb6747d51efa969d133e6ab8a33
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-06-09 16:38:54 +0900

    [Silver III] Title: 크면서 작은 수, Time: 80 ms, Memory: 11456 KB -BaekjoonHub

---
+++ 백준/Silver/2992. 크면서 작은 수/크면서 작은 수.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[] arr = br.readLine().toCharArray();
+        if (!nextPermutation(arr)) {
+            System.out.println(0);
+        } else {
+            System.out.println(arr);
+        }
+        br.close();
+    }
+    static boolean nextPermutation(char[] arr) {
+        int i = arr.length - 1;
+        while (i > 0 && arr[i - 1] >= arr[i]) {
+            --i;
+        }
+        if (i == 0) {
+            return false;
+        }
+        int j = arr.length - 1;
+        while (arr[i - 1] >= arr[j]) {
+            --j;
+        }
+        swap(arr, j, i - 1);
+        int k = arr.length - 1;
+        while (i < k) {
+            swap(arr, i++, k--);
+        }
+        return true;
+    }
+
+    static void swap(char[] arr, int a, int b) {
+        char tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
