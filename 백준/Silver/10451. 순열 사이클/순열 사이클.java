cafc6df47a56efa550025ac6d119e0b5a45d209d
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-06-22 02:29:50 +0900

    [Silver III] Title: 순열 사이클, Time: 376 ms, Memory: 53588 KB -BaekjoonHub

--- 백준/Silver/10451. 순열 사이클/순열 사이클.java
+++ 백준/Silver/10451. 순열 사이클/순열 사이클.java
@@ -1,11 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static List<Integer>[] adjList;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int tc = Integer.parseInt(br.readLine());
@@ -13,49 +11,28 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         while (tc-- > 0) {
             int n = Integer.parseInt(br.readLine());
+            int[] arr = new int[n + 1];
             st = new StringTokenizer(br.readLine());
-            adjList = new ArrayList[n + 1];
             for (int i = 1; i < n + 1; i++) {
-                adjList[i] = new ArrayList<>();
-                adjList[i].add(Integer.parseInt(st.nextToken()));
+                arr[i] = Integer.parseInt(st.nextToken());
             }
             boolean[] visited = new boolean[n + 1];
             int cnt = 0;
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i < n + 1; i++) {
                 if (visited[i]) {
                     continue;
                 }
-                if (bfs(i, visited)) {
-                    cnt++;
+                ++cnt;
+                int now = i;
+                while (!visited[now]) {
+                    visited[now] = true;
+                    now = arr[now];
                 }
             }
             sb.append(cnt).append("\n");
         }
         System.out.print(sb);
-
         br.close();
     }
 
-    static boolean bfs(int start, boolean[] visited) {
-        Queue<Integer> queue = new LinkedList<>();
-        Stack<Integer> stack = new Stack<>();
-        queue.add(start);
-        visited[start] = true;
-        while (!queue.isEmpty()) {
-            int now = queue.poll();
-            stack.push(now);
-            int next = adjList[now].get(0);
-            if (visited[next]) {
-                while (!stack.isEmpty()) {
-                    if (stack.pop() == next) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-            visited[next] = true;
-            queue.add(next);
-        }
-        return false;
-    }
 }
