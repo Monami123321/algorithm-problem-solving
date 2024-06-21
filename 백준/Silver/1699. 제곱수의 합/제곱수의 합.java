fb4d4d25c5625f4e4861eaebf4ab583fd82302ed
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-06-21 15:30:12 +0900

    [Silver II] Title: 제곱수의 합, Time: 140 ms, Memory: 12064 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -6,21 +6,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n + 1];
-        arr[0] = arr[1] = 1;
-        for (int i = 2; i <= n; i++) {
-            int sqrt = (int) Math.sqrt(i);
-            if (sqrt * sqrt == i) {
-                arr[i] = 1;
-                continue;
-            }
-            int limit = i >> 1;
-            arr[i] = arr[i - 1] + 1;
-            for (int j = 1; j <= limit; j++) {
-                arr[i] = Math.min(arr[i], arr[j] + arr[i - j]);
+        int[] dp = new int[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i - j * j] + 1, dp[i]);
             }
         }
-        System.out.println(arr[n]);
+        System.out.println(dp[n]);
         br.close();
     }
 }
