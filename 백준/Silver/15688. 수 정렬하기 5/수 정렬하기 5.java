6cd42b142e882c7bc480a00eefb88c2ed3f03402
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-06-26 16:57:55 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 25724 ms, Memory: 191140 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        mergeSort(arr);
+        System.out.print(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining("\n")));
+        br.close();
+    }
+
+    static void mergeSort(int[] arr) {
+        int[] tmp = new int[arr.length];
+        mergeSort(arr, tmp, 0, arr.length - 1);
+    }
+
+    static void mergeSort(int[] arr, int[] tmp, int left, int right) {
+        if (left == right) {
+            return;
+        }
+        int mid = left + right >> 1;
+        mergeSort(arr, tmp, left, mid);
+        mergeSort(arr, tmp, mid + 1, right);
+        merge(arr, tmp, left, mid, right);
+    }
+
+    static void merge(int[] arr, int[] tmp, int left, int mid, int right) {
+        int index = left;
+        int l = left;
+        int r = mid + 1;
+        while (l <= mid && r <= right) {
+            if (arr[l] < arr[r]) {
+                tmp[index++] = arr[l++];
+            } else {
+                tmp[index++] = arr[r++];
+            }
+        }
+        while (l <= mid) {
+            tmp[index++] = arr[l++];
+        }
+        while (r <= right) {
+            tmp[index++] = arr[r++];
+        }
+        System.arraycopy(tmp, left, arr, left, right - left + 1);
+    }
+}
