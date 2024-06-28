132ce3805e9d1809a96a334d73c2b056a94b4d02
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-06-29 00:48:59 +0900

    [Bronze II] Title: 8진수 2진수, Time: 252 ms, Memory: 24508 KB -BaekjoonHub

--- 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
+++ 백준/Bronze/1212. 8진수 2진수/8진수 2진수.java
@@ -1,13 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BigInteger n = new BigInteger(br.readLine(), 8);
-        System.out.print(n.toString(2));
+        char[] arr = br.readLine().toCharArray();
+        if (arr[0] == 48) {
+            System.out.println(0);
+            return;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length - 1; i >= 0; --i) {
+            int now = arr[i] - 48;
+            int cnt = 0;
+            while (now > 0) {
+                sb.append(now % 2);
+                now >>= 1;
+                ++cnt;
+            }
+            while (cnt++ < 3) {
+                sb.append(0);
+            }
+        }
+        sb.reverse();
+        while (sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+        System.out.println(sb);
         br.close();
     }
 }
