86e4554958d0cf18ac17ab22a42d427008bb8444
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-07-01 22:54:47 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,12 @@
+a = input()
+ans = None
+if a == "SONGDO":
+    ans = "HIGHSCHOOL"
+elif a == "CODE":
+    ans = "MASTER"
+elif a == "2023":
+    ans = "0611"
+else:
+    ans = "CONTEST"
+print(ans)
+    
