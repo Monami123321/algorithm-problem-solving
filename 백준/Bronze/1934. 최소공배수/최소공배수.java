5f95fe96db2bdaceacdf502766e3174968dd6540
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-08-29 09:52:22 +0900

    [Bronze I] Title: 최소공배수, Time: 80 ms, Memory: 12176 KB -BaekjoonHub

---
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        while (n-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(a * b / getGCD(a, b)).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+
+    static int getGCD(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+
+}
