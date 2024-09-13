69fa622f389834ff9f5f21714bc5500eccb82f62
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-09-13 19:38:45 +0900

    [Gold V] Title: 로봇 청소기, Time: 72 ms, Memory: 11844 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,83 +1,75 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dr = { -1, 0, 1, 0 };
-	static int[] dc = { 0, 1, 0, -1 };
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, 1, 0, -1};
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken()); // 세로
-		int m = Integer.parseInt(st.nextToken()); // 가로
-		st = new StringTokenizer(br.readLine());
-		int r = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		int dir = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int r = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int dir = Integer.parseInt(st.nextToken());
 
-		int[][] map = new int[n][m];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < m; j++) {
-				map[i][j] = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
 
-			}
+        int cnt = 0;
 
-		}
+        while (true) {
+            if (map[r][c] == 0) {
+                map[r][c] = 2;
+                cnt++;
+            }
 
-		int ans = 0;
-		boolean flag;
-		first: while (true) {
+            if (hasSpot(r, c, map)) {
+                for (; ; ) {
+                    dir = (dir + 3) % 4;
+                    int nr = r + dr[dir];
+                    int nc = c + dc[dir];
+                    if (map[nr][nc] == 0) {
+                        r = nr;
+                        c = nc;
+                    }
+                    break;
+                }
+            } else {
+                int nr = r + dr[(dir + 2) % 4];
+                int nc = c + dc[(dir + 2) % 4];
+                if (map[nr][nc] == 1) {
+                    break;
+                } else {
+                    r = nr;
+                    c = nc;
+                }
+            }
+        }
+        System.out.println(cnt);
 
-			if (map[r][c] == 0) {
-				map[r][c] = -1; // 청소 ㅡ> -1칠하기
-				ans++;
-			}
-			flag = false;
-			for (int i = 0; i < 4; i++) {
-				int nr = r + dr[i];
-				int nc = c + dc[i];
 
-				if (map[nr][nc] == 0) {
-					flag = true;
-					break;
-				}
+        br.close();
+    }
 
-			} // 상하좌우 청소 안 한 칸이 있는지 확인 ㅡ> flag
-
-			if (flag) { // 청소 안한 칸이 있음
-				while (true) {
-					dir = (dir + 3) % 4; // 반시계방향 회전
-					int nr = r + dr[dir];
-					int nc = c + dc[dir];
-					if (map[nr][nc] == 0) {
-						r = nr;
-						c = nc;
-						break;
-					} // 한칸 전진 , 반복문 탈출
-
-				}
-				continue first;
-
-			} else { // 청소안한 칸이 없을 떄
-				int nr = r + dr[(dir + 2) % 4];
-				int nc = c + dc[(dir + 2) % 4];
-				if (map[nr][nc] != 1) { // 후진 가능하면 후진
-					r = nr;
-					c = nc;
-					continue first;
-				} else { // 후진 불가능 ㅡ> 끝
-					break first;
-				}
-
-			}
-
-		}
-
-		System.out.println(ans);
-
-		br.close();
-	}
+    static boolean hasSpot(int r, int c, int[][] map) {
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+            if (map[nr][nc] == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
