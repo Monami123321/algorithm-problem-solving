94682187ecba85020e01addff7a1ed83ed156d0b
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-09-19 17:52:48 +0900

    [Gold V] Title: 신촌 통폐합 계획, Time: 576 ms, Memory: 161972 KB -BaekjoonHub

--- 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
+++ 백준/Gold/31423. 신촌 통폐합 계획/신촌 통폐합 계획.java
@@ -10,7 +10,6 @@ public class Main {
         String[] arr = new String[n + 1];
         int[] next = new int[n + 1];
         int[] last = new int[n + 1];
-        boolean[] visited = new boolean[n + 1];
 
         for (int i = 1; i < n + 1; i++) {
             arr[i] = br.readLine();
@@ -19,27 +18,21 @@ public class Main {
         }
 
         StringTokenizer st;
-
-
+        int from = 0;
+        int to = 0;
         for (int i = 0; i < n - 1; i++) {
             st = new StringTokenizer(br.readLine());
-            int from = Integer.parseInt(st.nextToken());
-            int to = Integer.parseInt(st.nextToken());
+            from = Integer.parseInt(st.nextToken());
+            to = Integer.parseInt(st.nextToken());
             next[last[from]] = to;
-            last[from] = last[next[last[from]]];
-            visited[to] = true;
+            last[from] = last[to];
         }
-        int start = 0;
-        for (int i = 1; i < n + 1; i++) {
-            if (!visited[i]) {
-                start = i;
-                break;
-            }
-        }
+
+
         StringBuilder sb = new StringBuilder();
         while (n-- > 0) {
-            sb.append(arr[start]);
-            start = next[start];
+            sb.append(arr[from]);
+            from = next[from];
         }
         System.out.print(sb);
         br.close();
