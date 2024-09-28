73699563c5df2f3b229bcc79bdf56e224485bd0c
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-09-28 22:11:55 +0900

    [Silver V] Title: 중복된 숫자, Time: 1092 ms, Memory: 21528 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.java
@@ -1,17 +1,19 @@
+import java.io.BufferedInputStream;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        BufferedInputStream bis = new BufferedInputStream(System.in);
         int tmp;
         int n = 0;
-        while ((tmp = System.in.read()) != '\n') {
+        while ((tmp = bis.read()) != '\n') {
             n *= 10;
             n += tmp - 48;
         }
         boolean[] arr = new boolean[n];
         n = 0;
-        while ((tmp = System.in.read()) != -1) {
-            if (tmp == '\n' || tmp == ' ') {
+        while ((tmp = bis.read()) != '\n') {
+            if (tmp == ' ') {
                 if (arr[n]) {
                     System.out.println(n);
                     return;
