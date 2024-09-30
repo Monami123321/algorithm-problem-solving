e463662a2cad50af047dfca995a5f6cce32327ca
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-09-30 09:27:33 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1212 ms, Memory: 454660 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -1,44 +1,62 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Main {
-    static int[] countingSort(int[] arr) {
-        int[] sortedArr = new int[arr.length];
+    static byte[] buffer = new byte[60000010];
+    static int index = 0;
+
+    public static void main(String[] args) throws IOException {
+        System.in.read(buffer);
+        int n = nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = nextInt();
+        }
+        countingSort(arr);
+        index = 0;
+        StringBuilder sb = new StringBuilder();
+        while (index < n) {
+            index++;
+            sb.append(arr[index - 1]).append("\n");
+            if (index % 1000 == 0) {
+                System.out.print(sb);
+                sb = new StringBuilder();
+            }
+        }
+        System.out.print(sb);
+    }
+
+    static int nextInt() {
+        int res = buffer[index++] - 48;
+        int tmp;
+
+        while (true) {
+            tmp = buffer[index++];
+            if (tmp < '0' || tmp > '9') {
+                break;
+            }
+            res *= 10;
+            res += tmp - 48;
+        }
+        return res;
+    }
+
+    static void countingSort(int[] arr) {
         int max = 0;
         for (int i = 0; i < arr.length; i++) {
             max = Math.max(max, arr[i]);
         }
-        int[] count = new int[max + 1];
+        int[] cnt = new int[max + 1];
         for (int i = 0; i < arr.length; i++) {
-            count[arr[i]]++;
+            cnt[arr[i]]++;
         }
-        for (int i = 1; i < max + 1; i++) {
-            count[i] += count[i - 1];
+        for (int i = 1; i < cnt.length; i++) {
+            cnt[i] += cnt[i - 1];
         }
+        int[] sorted = new int[arr.length];
+
         for (int i = arr.length - 1; i >= 0; i--) {
-            sortedArr[--count[arr[i]]] = arr[i];
+            sorted[--cnt[arr[i]]] = arr[i];
         }
-        return sortedArr;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        int[] res = countingSort(arr);
-        for (int i = 0; i < n; i++) {
-            sb.append(res[i]).append("\n");
-
-        }
-        System.out.print(sb);
-
-
-        br.close();
+        System.arraycopy(sorted, 0, arr, 0, arr.length);
     }
 }
