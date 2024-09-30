1c388ced0a86f7cc31184c06c11bb46ca1e9c53e
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-09-30 13:02:26 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 560 ms, Memory: 83676 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.java
@@ -1,59 +1,76 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class Main {
+    static byte[] buffer = new byte[8000009];
+    static int index = 0;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
+        System.in.read(buffer);
+        int n = nextInt();
         int[] arr = new int[n];
-
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            arr[i] = nextInt();
         }
-        quickSort(arr);
-        System.out.print(Arrays.stream(arr).mapToObj(String::valueOf).collect(Collectors.joining("\n")));
-        br.close();
+        heapSort(arr);
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(arr[i]).append("\n");
+        }
+        System.out.print(sb);
+
     }
 
-    static void quickSort(int[] arr) {
-        quickSort(arr, 0, arr.length - 1);
-    }
-
-    static void quickSort(int[] arr, int left, int right) {
-        int pivot = partition(arr, left, right);
-
-        if (pivot - 1 > left) {
-            quickSort(arr, left, pivot - 1);
+    static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = (n >> 1) - 1; i >= 0; i--) {
+            heapify(arr, n, i);
         }
-        if (pivot < right) {
-            quickSort(arr, pivot, right);
+
+        for (int i = n - 1; i > 0; i--) {
+            swap(arr, 0, i);
+            heapify(arr, i, 0);
         }
     }
 
-    static int partition(int[] arr, int start, int end) {
-        int val = arr[start + end >> 1];
-
-        while (start <= end) {
-            while (arr[start] > val) {
-                start++;
-            }
-            while (arr[end] < val) {
-                end--;
-            }
-            if (start <= end) {
-                swap(arr, start++, end--);
-            }
+    static void heapify(int[] arr, int size, int node) {
+        int left = node << 1 | 1;
+        int right = (node << 1) + 2;
+        int smallest = node;
+        if (left < size && arr[left] < arr[smallest]) {
+            smallest = left;
         }
-        return start;
+        if (right < size && arr[right] < arr[smallest]) {
+            smallest = right;
+        }
+        if (smallest != node) {
+            swap(arr, smallest, node);
+            heapify(arr, size, smallest);
+        }
     }
 
     static void swap(int[] arr, int a, int b) {
         int tmp = arr[a];
         arr[a] = arr[b];
         arr[b] = tmp;
+    }
+
+
+    static int nextInt() {
+        int res = buffer[index++];
+        boolean flag = res == '-';
+        if (flag) {
+            res = buffer[index++];
+        }
+        res -= 48;
+        int tmp;
+        while (true) {
+            tmp = buffer[index++];
+            if (tmp < '0' || tmp > '9') {
+                break;
+            }
+            res *= 10;
+            res += tmp - 48;
+        }
+        return flag ? -res : res;
     }
 }
