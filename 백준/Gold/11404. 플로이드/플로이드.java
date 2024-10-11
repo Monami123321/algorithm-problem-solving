f40b97af9068060fa7812a82cb24cec9da7a80c3
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-11 12:30:01 +0900

    [Gold IV] Title: 플로이드, Time: 288 ms, Memory: 41812 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,64 +1,53 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(br.readLine());
+    static int INF = 1 << 29;
 
-		int M = Integer.parseInt(br.readLine());
-		int INF = 100000 * 100;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int k = Integer.parseInt(br.readLine());
+        int[][] dist = new int[n + 1][n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
 
-		int[][] dist = new int[N + 1][N + 1];
-		for (int i = 1; i < dist.length; i++) {
-			for (int j = 1; j < dist.length; j++) {
-				dist[i][j] = INF;
+        StringTokenizer st;
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            if (dist[a][b] > c) {
+                dist[a][b] = c;
+            }
+        }
 
-			}
-			dist[i][i] = 0;
-
-		}
-
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			if (dist[a][b] > c)
-				dist[a][b] = c;
-
-		}
-
-		for (int m = 1; m < dist.length; m++) {
-			for (int s = 1; s < dist.length; s++) {
-				for (int e = 1; e < dist.length; e++) {
-					if (dist[s][e] > dist[s][m] + dist[m][e]) {
-						dist[s][e] = dist[s][m] + dist[m][e];
-					}
-
-				}
-
-			}
-
-		}
-
-		for (int i = 1; i < dist.length; i++) {
-			for (int j = 1; j < dist.length; j++) {
-				if (dist[i][j] == INF) {
-					dist[i][j] = 0;
-				}
-				sb.append(dist[i][j]).append(" ");
-
-			}
-			sb.append("\n");
-
-		}
-		System.out.print(sb);
-
-		br.close();
-	}
-
+        for (int m = 0; m < n + 1; m++) {
+            for (int s = 0; s < n + 1; s++) {
+                for (int e = 0; e < n + 1; e++) {
+                    if (dist[s][e] > dist[s][m] + dist[m][e]) {
+                        dist[s][e] = dist[s][m] + dist[m][e];
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < n+1; i++) {
+            for (int j = 1; j < n+1; j++) {
+                if (dist[i][j] == INF) {
+                    dist[i][j] = 0;
+                }
+                sb.append(dist[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
 }
