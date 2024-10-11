da48e703d29ed7362db6e4d7e42745f6affbed05
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-11 12:44:54 +0900

    [Silver I] Title: 경로 찾기, Time: 108 ms, Memory: 14560 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,74 +1,43 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int n = Integer.parseInt(br.readLine());
-		int[][] adjMatrix = new int[n][n];
-		for (int i = 0; i < adjMatrix.length; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < adjMatrix.length; j++) {
-				adjMatrix[i][j] = Integer.parseInt(st.nextToken());
-
-			}
-
-		}
-
-		int[][] ansMatrix = new int[n][n];
-
-		Queue<Integer> queue = new LinkedList<>();
-
-		for (int i = 0; i < n; i++) {
-			boolean[] visited = new boolean[n];
-
-			for (int j = 0; j < adjMatrix.length; j++) {
-				if (adjMatrix[i][j] == 1) {
-					queue.add(j);
-					visited[j] = true;
-				}
-
-			}
-
-			while (!queue.isEmpty()) {
-				int node = queue.poll();
-
-				for (int j = 0; j < n; j++) {
-					if (visited[j] || adjMatrix[node][j] == 0) {
-						continue;
-					}
-					visited[j] = true;
-					queue.add(j);
-
-				}
-			}
-
-			for (int j = 0; j < visited.length; j++) {
-				if (visited[j]) {
-					ansMatrix[i][j] = 1;
-				}
-
-			}
-
-		}
-
-		for (int i = 0; i < ansMatrix.length; i++) {
-			for (int j = 0; j < ansMatrix.length; j++) {
-				sb.append(ansMatrix[i][j]).append(" ");
-
-			}
-			sb.append("\n");
-
-		}
-		System.out.println(sb);
-
-		br.close();
-	}
-
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] dp = new boolean[n][n];
+        StringTokenizer st;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                if (st.nextToken().equals("1")) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        for (int m = 0; m < n; m++) {
+            for (int s = 0; s < n; s++) {
+                for (int e = 0; e < n; e++) {
+                    if (!dp[s][e]) {
+                        dp[s][e] = dp[s][m] && dp[m][e];
+                    }
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (dp[i][j]) {
+                    sb.append(1).append(" ");
+                } else {
+                    sb.append(0).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
 }
