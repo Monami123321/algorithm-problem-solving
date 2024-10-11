99a51a9d65848d5571a955c6fe35241e3aea7fcf
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-11 12:05:14 +0900

    [Gold IV] Title: 최단경로, Time: 728 ms, Memory: 130792 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,62 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-
-		int start = Integer.parseInt(br.readLine());
-		List<int[]>[] adjList = new ArrayList[n + 1];
-		for (int i = 1; i < n + 1; i++) {
-			adjList[i] = new ArrayList<>();
-		}
-		for (int i = 0; i < m; i++) {
-			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
-			int cost = Integer.parseInt(st.nextToken());
-			adjList[s].add(new int[] {e, cost});
-		}
-		int[] dist = new int[n + 1];
-		boolean[] visited = new boolean[n + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-		pq.add(new int[] {start, 0});
-		int pick = 0;
-		while (!pq.isEmpty()) {
-			int[] now = pq.poll();
-			if (visited[now[0]]) {
-				continue;
-			}
-			visited[now[0]] = true;
-			dist[now[0]] = now[1];
-			adjList[now[0]].forEach(e -> {
-				if (visited[e[0]]) {
-					return;
-				}
-				pq.add(new int[] {e[0], e[1] + now[1]});
-			});
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i < n+1; i++) {
-			if (dist[i] == Integer.MAX_VALUE) {
-				sb.append("INF\n");
-			} else {
-				sb.append(dist[i]).append("\n");
-			}
-		}
-
-		System.out.print(sb);
-		br.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int start = Integer.parseInt(br.readLine());
+        List<int[]>[] adjList = new ArrayList[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            adjList[a].add(new int[]{b, c});
+        }
+        int[] visited = new int[n + 1];
+        Arrays.fill(visited, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        pq.add(new int[]{start, 0});
+        while (!pq.isEmpty()) {
+            int[] now = pq.poll();
+            int node = now[0];
+            int cost = now[1];
+            if (visited[node] != Integer.MAX_VALUE) {
+                continue;
+            }
+            visited[node] = cost;
+            adjList[node].forEach(e -> {
+                if (visited[e[0]] == Integer.MAX_VALUE) {
+                    pq.add(new int[]{e[0], cost + e[1]});
+                }
+            });
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < n + 1; i++) {
+            if (visited[i] == Integer.MAX_VALUE) {
+                sb.append("INF\n");
+            } else {
+                sb.append(visited[i]).append("\n");
+            }
+        }
+        System.out.print(sb);
+        br.close();
+    }
 }
-
