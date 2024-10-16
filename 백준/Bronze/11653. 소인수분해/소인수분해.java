00273f7f841ecef739022e9f5c12e04ec52a7cae
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-16 15:53:39 +0900

    [Bronze I] Title: 소인수분해, Time: 1612 ms, Memory: 41716 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,63 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    static byte[] buffer = new byte[8];
+
+    public static void main(String[] args) throws IOException {
+        int idx = System.in.read(buffer) - 1;
+        int n = buffer[0] - 48;
+        for (int i = 1; i < idx; i++) {
+            n *= 10;
+            n += buffer[i] - 48;
+        }
+        int limit = n;
+
+        List<Integer> list = new ArrayList<>();
+        boolean[] visited = new boolean[limit + 1];
+        for (int i = 2; i <= limit; i++) {
+            if (visited[i]) {
+                continue;
+            }
+            if (isPrime(i)) {
+                list.add(i);
+                idx = i;
+                while (idx <= limit) {
+                    visited[idx] = true;
+                    idx += i;
+                }
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        idx = 0;
+        while (n > 1) {
+            int p = list.get(idx++);
+            while (n % p == 0) {
+                n /= p;
+                sb.append(p).append("\n");
+            }
+        }
+        System.out.print(sb);
+    }
+
+    static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        int limit = (int) Math.sqrt(n);
+
+        for (int i = 6; i <= limit; i += 5) {
+            if (n % (i - 1) == 0 || n % (i + 1) == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
