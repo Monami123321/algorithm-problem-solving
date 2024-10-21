b02b6037eb5783fa5a1736f470cd0e8a2d3862a8
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-21 10:50:58 +0900

    [Silver I] Title: 토쟁이의 등굣길, Time: 68 ms, Memory: 11768 KB -BaekjoonHub

--- 백준/Silver/12785. 토쟁이의 등굣길/토쟁이의 등굣길.java
+++ 백준/Silver/12785. 토쟁이의 등굣길/토쟁이의 등굣길.java
@@ -1,31 +1,34 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-    public static void main(String[] args) throws Exception {
+    static final int MOD = 1000007;
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int W = Integer.parseInt(st.nextToken()) - 1;
-        int H = Integer.parseInt(st.nextToken()) - 1;
-        st = new StringTokenizer(br.readLine(), " ");
-        int X = Integer.parseInt(st.nextToken()) - 1;
-        int Y = Integer.parseInt(st.nextToken()) - 1;
-        long[][] dp = new long[200][200];
-        for (int i = 0; i < 200; i++) {
-            dp[i][0] = dp[0][i] = 1;
-        }
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken()) - 1;
+        int r = Integer.parseInt(st.nextToken()) - 1;
 
-        for (int i = 1; i < 200; i++) {
-            for (int j = 1; j < 200; j++) {
-                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1_000_007;
+        long[][] dp = new long[n][m];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            dp[0][i] = 1;
+        }
+        dp[0][0] = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD;
             }
         }
-
-        System.out.print((dp[X][Y] * dp[W - X][H - Y]) % 1_000_007);
-
+        System.out.println(dp[r][c] * dp[n - 1 - r][m - 1 - c] % MOD);
+        br.close();
     }
-	
 }
