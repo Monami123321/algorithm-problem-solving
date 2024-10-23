d234f04eee65e3676e642932452d6bdb5e8f13ff
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-23 15:06:39 +0900

    [Silver IV] Title: 대피소, Time: 72 ms, Memory: 11616 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/28215. 대피소/대피소.java
@@ -0,0 +1,88 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] dist = new int[n][n];
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int[] tmp1 = arr[i];
+                int[] tmp2 = arr[j];
+                dist[j][i] = dist[i][j] = getDist(tmp1[0], tmp1[1], tmp2[0], tmp2[1]);
+            }
+        }
+        int[] selected = new int[n];
+        for (int i = 0; i < k; i++) {
+            selected[n - 1 - i] = 1;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        do {
+            int caseAns = 0;
+            int index = 0;
+            int[] tmp = new int[k];
+            for (int i = 0; i < n; i++) {
+                if (selected[i] != 0) {
+                    tmp[index++] = i;
+                }
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < k; j++) {
+                    if (selected[i] == 1) {
+                        continue;
+                    }
+                    int shelter = tmp[j];
+                    caseAns = Math.max(dist[shelter][i], caseAns);
+                }
+            }
+            ans = Math.min(ans, caseAns);
+        } while (nextPerm(selected));
+        System.out.println(ans);
+
+        br.close();
+    }
+
+    static int getDist(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    }
+
+    static boolean nextPerm(int[] arr) {
+        int i = arr.length - 1;
+        while (i > 0 && arr[i - 1] >= arr[i]) {
+            i--;
+        }
+        if (i == 0) {
+            return false;
+        }
+        int j = arr.length - 1;
+        while (arr[i - 1] >= arr[j]) {
+            j--;
+        }
+        swap(arr, i - 1, j);
+        int k = arr.length - 1;
+        while (i < k) {
+            swap(arr, i++, k--);
+        }
+        return true;
+    }
+
+    static void swap(int[] arr, int a, int b) {
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
+    }
+}
