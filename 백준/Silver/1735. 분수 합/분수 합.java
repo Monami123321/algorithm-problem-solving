3b3db8cb97822da2e5e5056d428e2d958444d5a6
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-28 17:19:04 +0900

    [Silver III] Title: 분수 합, Time: 76 ms, Memory: 11748 KB -BaekjoonHub

---
+++ 백준/Silver/1735. 분수 합/분수 합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int num = a * d + b * c;
+        int denom = b * d;
+        int gcd = getGCD(num, denom);
+        System.out.printf("%d %d", num / gcd, denom / gcd);
+        br.close();
+    }
+
+    static int getGCD(int a, int b) {
+        while (b != 0) {
+            int tmp = a % b;
+            a = b;
+            b = tmp;
+        }
+        return a;
+    }
+}
