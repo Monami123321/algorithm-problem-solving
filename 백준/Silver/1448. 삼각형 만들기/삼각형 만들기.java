0dc3b9d600a014cc70077a88b79b654267a27047
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-10-31 10:21:02 +0900

    [Silver III] Title: 삼각형 만들기, Time: 612 ms, Memory: 80392 KB -BaekjoonHub

---
+++ 백준/Silver/1448. 삼각형 만들기/삼각형 만들기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+        int ans = -1;
+        for (int i = arr.length - 1; i >= 2; i--) {
+            if (arr[i] < arr[i - 1] + arr[i - 2]) {
+                ans = arr[i] + arr[i - 1] + arr[i - 2];
+                break;
+            }
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
