bba51634e4baf0f7155bc29b4182afb7e6c43f3d
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-03 17:02:47 +0900

    [Silver IV] Title: 덱 2, Time: 1024 ms, Memory: 257992 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.java
+++ 백준/Silver/28279. 덱 2/덱 2.java
@@ -1,70 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[3000000];
-        int left = arr.length >> 1;
-        int right = left + 1;
-        int size = 0;
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+        Deque<Integer> deque = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            switch (st.nextToken()) {
-                case "1":
-                    arr[left--] = Integer.parseInt(st.nextToken());
-                    size++;
-                    break;
-                case "2":
-                    arr[right++] = Integer.parseInt(st.nextToken());
-                    size++;
-                    break;
-                case "3":
-                    if (size == 0) {
-                        sb.append("-1\n");
-                    } else {
-                        size--;
-                        sb.append(arr[++left]).append("\n");
-                    }
-                    break;
-                case "4":
-                    if (size == 0) {
-                        sb.append("-1\n");
-                    } else {
-                        size--;
-                        sb.append(arr[--right]).append("\n");
-                    }
-                    break;
-                case "5":
-                    sb.append(size).append("\n");
-                    break;
-                case "6":
-                    if (size == 0) {
-                        sb.append("1\n");
-                    } else {
-                        sb.append("0\n");
-                    }
-                    break;
-                case "7":
-                    if (size == 0) {
-                        sb.append("-1\n");
-                    } else {
-                        sb.append(arr[left + 1]).append("\n");
-                    }
-                    break;
-                case "8":
-                    if (size == 0) {
-                        sb.append("-1\n");
-                    } else {
-                        sb.append(arr[right - 1]).append("\n");
-                    }
-                    break;
+            int cmd = Integer.parseInt(st.nextToken());
+            if (cmd == 1) {
+                deque.addFirst(Integer.parseInt(st.nextToken()));
+            } else if (cmd == 2) {
+                deque.addLast(Integer.parseInt(st.nextToken()));
+            } else if (cmd == 3) {
+                if (deque.isEmpty()) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(deque.pollFirst()).append("\n");
+                }
+            } else if (cmd == 4) {
+                if (deque.isEmpty()) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(deque.pollLast()).append("\n");
+                }
+            } else if (cmd == 5) {
+                sb.append(deque.size()).append("\n");
+            } else if (cmd == 6) {
+                if (deque.isEmpty()) {
+                    sb.append("1\n");
+                } else {
+                    sb.append("0\n");
+                }
+            } else if (cmd == 7) {
+                if (deque.isEmpty()) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(deque.getFirst()).append("\n");
+                }
+            } else {
+                if (deque.isEmpty()) {
+                    sb.append("-1\n");
+                } else {
+                    sb.append(deque.getLast()).append("\n");
+                }
             }
         }
         System.out.print(sb);
