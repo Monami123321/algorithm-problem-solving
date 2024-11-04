a01dbd60bae74ce6e7a7b86b26d7b52407ad30a6
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-04 14:46:43 +0900

    [Gold IV] Title: 어려운 모든 정점 쌍 최단 거리, Time: 392 ms, Memory: 66080 KB -BaekjoonHub

--- 백준/Gold/23324. 어려운 모든 정점 쌍 최단 거리/어려운 모든 정점 쌍 최단 거리.java
+++ 백준/Gold/23324. 어려운 모든 정점 쌍 최단 거리/어려운 모든 정점 쌍 최단 거리.java
@@ -1,49 +1,56 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int[] parent, cnt;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        List<Integer>[] adjList = new ArrayList[n + 1];
+        parent = new int[n + 1];
+        cnt = new int[n + 1];
         for (int i = 1; i < n + 1; i++) {
-            adjList[i] = new ArrayList<>();
+            parent[i] = i;
+            cnt[i] = 1;
         }
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             if (i == k - 1) {
                 continue;
             }
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            adjList[a].add(b);
-            adjList[b].add(a);
+            union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
         }
-        Queue<Integer> queue = new ArrayDeque<>();
-        long cnt = bfs(adjList, queue, 1, new boolean[n + 1]);
-        System.out.println(cnt * (n - cnt));
+        long l = cnt[findSet(1)];
+        System.out.println(l * (n - l));
         br.close();
     }
 
-    static int bfs(List<Integer>[] adjList, Queue<Integer> queue, int start, boolean[] visited) {
-        queue.add(start);
-        visited[start] = true;
-        int cnt = 1;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int n : adjList[node]) {
-                if (!visited[n]) {
-                    cnt++;
-                    queue.add(n);
-                    visited[n] = true;
-                }
-            }
+    static int findSet(int a) {
+        if (parent[a] != a) {
+            return parent[a] = findSet(parent[a]);
         }
-        return cnt;
+        return a;
+    }
+
+    static void union(int a, int b) {
+        int parentA = findSet(a);
+        int parentB = findSet(b);
+        if (parentA == parentB) {
+            return;
+        }
+
+        if (parentA < parentB) {
+            parent[parentB] = parentA;
+            cnt[parentA] += cnt[parentB];
+        } else {
+            parent[parentA] = parentB;
+            cnt[parentB] += cnt[parentA];
+        }
     }
 }
+
