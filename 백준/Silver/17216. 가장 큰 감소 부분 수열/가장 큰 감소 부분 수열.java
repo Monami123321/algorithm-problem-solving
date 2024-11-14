2cd9a363173909e809d9d89ebc288ade8a8ae6ca
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-14 22:22:07 +0900

    [Silver II] Title: 가장 큰 감소 부분 수열, Time: 84 ms, Memory: 12132 KB -BaekjoonHub

---
+++ 백준/Silver/17216. 가장 큰 감소 부분 수열/가장 큰 감소 부분 수열.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int ans = dp[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = n - 1; j > i; j--) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(ans);
+        br.close();
+    }
+}
