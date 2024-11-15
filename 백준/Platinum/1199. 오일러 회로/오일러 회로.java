effe8f1ab110880285e5ee197bf71c1094af0282
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-15 11:07:02 +0900

    [Platinum IV] Title: 오일러 회로, Time: 3684 ms, Memory: 747432 KB -BaekjoonHub

--- 백준/Platinum/1199. 오일러 회로/오일러 회로.java
+++ 백준/Platinum/1199. 오일러 회로/오일러 회로.java
@@ -1,44 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int n;
+    static int[][] adjMatrix;
+    static StringBuilder sb;
 
-	static int n;
-	static int[][] arr;
-	static StringBuilder sb;
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-		arr = new int[n][n];
-		StringTokenizer st;
-        
-		for(int i=0; i<n; i++) {
-			int vertex = 0;
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<n; j++) {
-				arr[i][j] = Integer.parseInt(st.nextToken());
-				vertex += arr[i][j];
-			}
-			if(vertex%2!=0) {
-				System.out.println(-1);
-				return;
-			}
-		}
-		
-		sb = new StringBuilder();
-		getEulerCircuit(0);
-		System.out.println(sb.toString());
-	}
-	
-	static void getEulerCircuit(int cur) {
-		for(int nxt=0; nxt<arr.length; nxt++) {
-			while(arr[cur][nxt] > 0) {
-				arr[cur][nxt]--;
-				arr[nxt][cur]--;
-				getEulerCircuit(nxt);
-			}
-		}
-		sb.append((cur+1)+" ");
-	}
+        StringTokenizer st;
+        adjMatrix = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                cnt += adjMatrix[i][j] = Integer.parseInt(st.nextToken());
+            }
+            if ((cnt & 1) != 0) {
+                System.out.println(-1);
+                return;
+            }
+        }
+        sb = new StringBuilder();
+        dfs(0);
+        System.out.print(sb);
+        br.close();
+    }
+
+    static void dfs(int node) {
+        for (int i = 0; i < adjMatrix.length; i++) {
+            while (adjMatrix[node][i] > 0) {
+                adjMatrix[node][i]--;
+                adjMatrix[i][node]--;
+                dfs(i);
+            }
+        }
+        sb.append((node + 1) + " ");
+    }
 }
