ca0190dd5092b6745886d7b8f7e06687292334e4
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-15 16:44:10 +0900

    [Platinum V] Title: 팀 편성, Time: 400 ms, Memory: 115840 KB -BaekjoonHub

--- 백준/Platinum/1154. 팀 편성/팀 편성.java
+++ 백준/Platinum/1154. 팀 편성/팀 편성.java
@@ -1,73 +1,57 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static List<Integer>[] adjList;
+    static int n;
+    static boolean[][] adjMatrix;
     static int[] visited;
     static boolean flag = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        boolean[][] arr = new boolean[n + 1][n + 1];
+        n = Integer.parseInt(br.readLine());
+        visited = new int[n];
+        adjMatrix = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            visited[i] = -1;
+        }
         StringTokenizer st;
         while (true) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            if (a == -1) {
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            if (a == -2) {
                 break;
             }
-            int b = Integer.parseInt(st.nextToken());
-            arr[a][b] = arr[b][a] = true;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            adjMatrix[a][b] = true;
+            adjMatrix[b][a] = true;
         }
-        adjList = new ArrayList[n + 1];
-        visited = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            adjList[i] = new ArrayList<>();
-            visited[i] = -1;
-        }
-        for (int i = 1; i < n; i++) {
-            for (int j = i + 1; j < n + 1; j++) {
-                if (!arr[i][j]) {
-                    adjList[i].add(j);
-                    adjList[j].add(i);
-                }
-            }
-        }
-        for (int i = 1; i < n + 1; i++) {
+        for (int i = 0; i < n; i++) {
             if (flag) {
-                break;
+                System.out.println(-1);
+                return;
             }
             if (visited[i] == -1) {
                 dfs(i, 0);
             }
         }
-        StringBuilder sb = new StringBuilder();
-        List<Integer> list1 = new ArrayList<>();
-        List<Integer> list2 = new ArrayList<>();
-        if (flag) {
-            sb.append(-1);
-        } else {
-            sb.append(1).append("\n");
-            for (int i = 1; i < n + 1; i++) {
-                if (visited[i] == 0) {
-                    list1.add(i);
-                } else {
-                    list2.add(i);
-                }
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            if (visited[i] == 0) {
+                sb1.append(i + 1).append(" ");
+            } else {
+                sb2.append(i + 1).append(" ");
             }
-
-            list1.forEach(e -> sb.append(e).append(" "));
-            sb.append(-1).append("\n");
-            list2.forEach(e -> sb.append(e).append(" "));
-            sb.append(-1).append("\n");
-
         }
-        System.out.print(sb);
+        sb1.append(-1);
+        sb2.append(-1);
+        System.out.println(1);
+        System.out.println(sb1);
+        System.out.println(sb2);
+
         br.close();
     }
 
@@ -76,15 +60,16 @@ public class Main {
             return;
         }
         visited[node] = mark;
-
-        for (int c : adjList[node]) {
-            if (visited[c] == -1) {
-                dfs(c, mark ^ 1);
-            } else {
-                if (visited[c] == mark) {
-                    flag = true;
-                    return;
-                }
+        for (int i = 0; i < n; i++) {
+            if (adjMatrix[node][i] || node == i) {
+                continue;
+            }
+            if (visited[i] == mark) {
+                flag = true;
+                return;
+            }
+            if (visited[i] == -1) {
+                dfs(i, mark ^ 1);
             }
         }
     }
