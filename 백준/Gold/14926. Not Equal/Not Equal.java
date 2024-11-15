a9ad885114d043bd8e9f456e11ef8a1310833456
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-15 15:22:38 +0900

    [Gold V] Title: Not Equal, Time: 1500 ms, Memory: 161180 KB -BaekjoonHub

---
+++ 백준/Gold/14926. Not Equal/Not Equal.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] visited = new boolean[n][n];
+        visited[0][n - 1] = visited[n - 1][0] = true;
+        StringBuilder sb = new StringBuilder("a1 ");
+        int len = ((n * (n - 1)) >> 1) - 1;
+        int cnt = 0;
+        int now = 0;
+        while (cnt < len) {
+            for (int i = 0; i < n; i++) {
+                if (visited[now][i] || now == i) {
+                    continue;
+                }
+                visited[now][i] = visited[i][now] = true;
+                now = i;
+                sb.append(String.format("a%d ", now + 1));
+                cnt++;
+                break;
+            }
+        }
+        sb.append("a1");
+        System.out.println(sb);
+        br.close();
+    }
+}
