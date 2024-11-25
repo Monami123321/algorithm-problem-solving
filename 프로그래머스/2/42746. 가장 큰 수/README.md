baa0317bf768e7df6d315bbd373638353a05fbee
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-25 13:21:21 +0900

    [level 2] Title: 가장 큰 수, Time: 259.67 ms, Memory: 127 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/README.md
+++ 프로그래머스/2/42746. 가장 큰 수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 가장 큰 수 - 42746 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42746) 
 
 ### 성능 요약
 
-메모리: 131 MB, 시간: 368.36 ms
+메모리: 127 MB, 시간: 259.67 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 25일 13:15:43
+2024년 11월 25일 13:21:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,9 +2,8 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public String solution(int[] numbers) {
-        int len = numbers.length;
-        String[] arr = Arrays.stream(numbers).mapToObj(String::valueOf).sorted((a, b) -> -(a + b).compareTo(b + a)).toArray(String[]::new);
-        return arr[0].equals("0") ? "0" : Arrays.stream(arr).collect(Collectors.joining(""));
+        String res = Arrays.stream(numbers).mapToObj(String::valueOf).sorted((a, b) -> -(a + b).compareTo(b + a)).collect(Collectors.joining(""));
+        return res.charAt(0) == 48 ? "0" : res;
     }
     
 }
