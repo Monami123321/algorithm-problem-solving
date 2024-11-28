f60c40a94700e0748060f767375f63433555d676
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-28 18:03:00 +0900

    [Silver III] Title: N과 M (2), Time: 64 ms, Memory: 11572 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -4,39 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static StringBuilder sb;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        combination(0, 0, new boolean[n]);
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+        combination(n, m, new boolean[n + 1], 0, 0, sb);
         System.out.print(sb);
-
+        br.close();
     }
 
-    static void combination(int depth, int pick, boolean[] visited) {
+    static void combination(int n, int m, boolean[] visited, int depth, int pick, StringBuilder sb) {
         if (pick == m) {
-            for (int i = 0; i < n; i++) {
+            for (int i = 1; i <= n; i++) {
                 if (visited[i]) {
-                    sb.append(i + 1).append(" ");
+                    sb.append(i).append(" ");
                 }
             }
             sb.append("\n");
             return;
         }
-        if (depth == n) {
+        if (n == depth) {
             return;
         }
-        visited[depth] = true;
-        pick++;
-        combination(depth + 1, pick, visited);
-        visited[depth] = false;
-        pick--;
-        combination(depth + 1, pick, visited);
+        visited[depth + 1] = true;
+        combination(n, m, visited, depth + 1, pick + 1, sb);
+        visited[depth + 1] = false;
+        combination(n, m, visited, depth + 1, pick, sb);
     }
 }
