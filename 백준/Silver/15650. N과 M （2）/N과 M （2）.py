2dce65c43e631f0beabc840c1585e81f58855e2a
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-11-28 17:30:37 +0900

    [Silver III] Title: N과 M (2), Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,7 @@
+import sys, itertools
+
+n, m = map(int, sys.stdin.read().split())
+ans = []
+for i in itertools.combinations([x for x in range(1, n+1)], m):
+    ans.append(" ".join(map(str, i)))
+sys.stdout.write("\n".join(ans))    
