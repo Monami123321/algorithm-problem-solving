b6d06aae46cb713fa05b9879bfbc48c2f179620e
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-12-02 16:54:47 +0900

    [Silver III] Title: 소수 구하기, Time: 156 ms, Memory: 19028 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,44 +1,56 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static boolean isPrime(int x) {
-        for (int i = 2; i <= Math.sqrt(x); i++) {
-            if (x % i == 0) {
-                return false;
-            }
-
-        }
-        return true;
-
-    }
-
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
         boolean[] visited = new boolean[n + 1];
-        visited[0] = visited[1] = true;
-
-        for (int i = m; i < n + 1; i++) {
-            if (visited[i])
+        int limit = (int) Math.sqrt(n);
+        for (int i = 1; i <= limit; i++) {
+            if (visited[i]) {
                 continue;
+            }
             if (isPrime(i)) {
-                sb.append(i).append("\n");
-                int next = i * 2;
-                while (next <= n) {
-                    visited[next] = true;
-                    next += i;
+                int index = i << 1;
+                while (index <= n) {
+                    visited[index] = true;
+                    index += i;
                 }
+            } else {
+                visited[i] = true;
             }
         }
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = m; i <= n; i++) {
+            if (!visited[i]) {
+                sb.append(i).append("\n");
+            }
+        }
         System.out.print(sb);
+        br.close();
+    }
 
-
+    static boolean isPrime(int n) {
+        if (n < 2) {
+            return false;
+        }
+        if (n <= 3) {
+            return true;
+        }
+        if ((n & 1) == 0 || n % 3 == 0) {
+            return false;
+        }
+        int limit = (int) Math.sqrt(n);
+        for (int i = 5; i <= limit; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
