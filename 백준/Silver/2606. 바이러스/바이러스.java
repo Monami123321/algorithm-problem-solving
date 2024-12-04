8710ba921cf0fbf245e5209218e197af59184c1d
Monami123321 <139312374+Monami123321@users.noreply.github.com>
2024-12-04 15:13:10 +0900

    [Silver III] Title: 바이러스, Time: 64 ms, Memory: 11640 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,46 +1,49 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static List<Integer>[] adjList;
-	static int ans;
+    static int[] parent, cnt;
 
-	static void dfs(int computerNo, boolean[] visited, List<Integer> adjNodes) {
-		visited[computerNo] = true;
-		for (int i = 0; i < adjNodes.size(); i++) {
-			if (visited[adjNodes.get(i)])
-				continue;
-			dfs(adjNodes.get(i), visited, adjList[adjNodes.get(i)]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        parent = new int[n + 1];
+        cnt = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+            cnt[i] = 1;
+        }
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            union(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+        System.out.println(cnt[1] - 1);
+        br.close();
+    }
 
-		}
-		ans++;
+    static int findSet(int a) {
+        if (parent[a] != a) {
+            return parent[a] = findSet(parent[a]);
+        }
+        return a;
+    }
 
-	}
-
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-
-		int v = scanner.nextInt();
-		int e = scanner.nextInt();
-
-		ans = 0;
-		adjList = new ArrayList[v + 1];
-		for (int i = 1; i < v + 1; i++) {
-			adjList[i] = new ArrayList<>();
-
-		}
-		for (int i = 0; i < e; i++) {
-			int x = scanner.nextInt();
-			int y = scanner.nextInt();
-			adjList[x].add(y);
-			adjList[y].add(x);
-
-		}
-		dfs(1, new boolean[v+1], adjList[1]);
-		System.out.println(ans-1);
-
-		scanner.close();
-	}
-
+    static void union(int a, int b) {
+        int parentA = findSet(a);
+        int parentB = findSet(b);
+        if (parentA == parentB) {
+            return;
+        }
+        if (parentA < parentB) {
+            parent[parentB] = parentA;
+            cnt[parentA] += cnt[parentB];
+        } else {
+            parent[parentA] = parentB;
+            cnt[parentB] += cnt[parentA];
+        }
+    }
 }
