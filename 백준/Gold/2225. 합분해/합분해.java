0dd5e9c6868d99e349fbe6dec34ca6dd7c0c4403
유호영 <139312374+Monami123321@users.noreply.github.com>
2024-12-30 17:23:20 +0900

    [Gold V] Title: 합분해, Time: 96 ms, Memory: 11988 KB -BaekjoonHub

---
+++ 백준/Gold/2225. 합분해/합분해.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static final int MOD = 1000000000;
+    static int[][] dp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        dp = new int[n + 1][k + 1];
+        System.out.println(getVal(n, k));
+        br.close();
+    }
+
+    static int getVal(int n, int k) {
+        if (k == 1 || n == 0) {
+
+            return 1;
+        }
+        if (dp[n][k] != 0) {
+            return dp[n][k];
+        }
+        int sum = 0;
+        for (int i = 0; i <= n; i++) {
+            sum += getVal(i, k - 1);
+            sum %= MOD;
+        }
+        return dp[n][k] = sum;
+    }
+
+
+}
+
