d735d05c8cba00918211b855c007e470238b6209
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-01-04 20:58:07 +0900

    [Gold I] Title: 이항 계수 3, Time: 108 ms, Memory: 42532 KB -BaekjoonHub

--- 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
+++ 백준/Gold/11401. 이항 계수 3/이항 계수 3.java
@@ -15,20 +15,22 @@ public class Main {
         dp = new long[n + 1];
         dp[0] = dp[1] = 1;
         for (int i = 2; i <= n; i++) {
-            dp[i] = (dp[i - 1] * i) % MOD;
+            dp[i] = dp[i - 1] * i % MOD;
         }
-        System.out.println((dp[n] * (recur(dp[k], MOD - 2) * recur(dp[n - k], MOD - 2) % MOD)) % MOD);
+        System.out.println(dp[n] * recurr(dp[n - k], MOD - 2) % MOD * recurr(dp[k], MOD - 2) % MOD);
         br.close();
     }
 
-    static long recur(long base, int exp) {
+    static long recurr(long base, int exp) {
         if (exp == 1) {
             return base;
         }
-        long half = recur(base, exp >> 1);
-        if ((exp & 1) == 0) {
-            return half * half % MOD;
+        long half = recurr(base, exp >> 1);
+        long res = half * half % MOD;
+        if ((exp & 1) != 0) {
+            res *= base;
+            res %= MOD;
         }
-        return ((half * half % MOD) * base) % MOD;
+        return res;
     }
 }
