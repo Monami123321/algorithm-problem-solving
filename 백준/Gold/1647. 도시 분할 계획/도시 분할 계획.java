147f82686048373583f8d4842312106f1014c785
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-01-12 02:55:35 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 2228 ms, Memory: 340284 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.java
@@ -1,12 +1,43 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
-
     static int[] parent;
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        parent = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+        int[][] arr = new int[m][3];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int index = 0;
+        int pick = 0;
+        int ans = 0;
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[2]));
+        while (pick < n - 1) {
+            if (union(arr[index][0], arr[index][1])) {
+                pick++;
+                ans += arr[index][2];
+            }
+            index++;
+        }
+        System.out.println(ans - arr[index - 1][2]);
+        br.close();
+    }
 
     static int findSet(int a) {
         if (parent[a] != a) {
@@ -15,60 +46,20 @@ public class Main {
         return a;
     }
 
-    static void union(int a, int b) {
-        if (a == b) {
-            return;
-        } else if (a > b) {
-            parent[findSet(a)] = findSet(b);
+    static boolean union(int a, int b) {
+        int parentA = findSet(a);
+        int parentB = findSet(b);
 
+        if (parentA == parentB) {
+            return false;
+        }
+
+        if (parentA < parentB) {
+            parent[parentB] = parentA;
         } else {
-            parent[findSet(b)] = findSet(a);
+            parent[parentA] = parentB;
         }
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-
-        parent = new int[n + 1];
-        for (int i = 1; i < n + 1; i++) {
-            parent[i] = i;
-        }
-
-        int[][] edges = new int[m][3];
-        for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            int c = Integer.parseInt(st.nextToken());
-            edges[i][0] = a;
-            edges[i][1] = b;
-            edges[i][2] = c;
-        }
-
-        Arrays.sort(edges, (a, b) -> a[2] - b[2]);
-
-        int pick = 0;
-        int cnt = 0;
-        int index = 0;
-        while (pick < n - 2) {
-            int start = edges[index][0];
-            int end = edges[index][1];
-            int cost = edges[index][2];
-
-            if (findSet(start) == findSet(end)) {
-                index++;
-            } else {
-                union(start, end);
-                index++;
-                cnt += cost;
-                pick++;
-            }
-        }
-        System.out.println(cnt);
-
-
+        return true;
     }
 }
+
