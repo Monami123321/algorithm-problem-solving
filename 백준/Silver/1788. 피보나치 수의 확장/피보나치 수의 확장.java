9d0c5e62868d066ddf8d4bbecef6090ded671e62
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-01-16 21:22:36 +0900

    [Silver III] Title: 피보나치 수의 확장, Time: 76 ms, Memory: 15324 KB -BaekjoonHub

--- 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
+++ 백준/Silver/1788. 피보나치 수의 확장/피보나치 수의 확장.java
@@ -1,20 +1,6 @@
 import java.io.IOException;
 
 public class Main {
-    static int[] arr = new int[2000001];
-    static final int MOD = 1000000000;
-
-    static {
-        arr[1000001] = 1;
-        for (int i = 2; i <= 1000000; i++) {
-            arr[1000000 + i] = (arr[1000000 + i - 1] + arr[1000000 + i - 2]) % MOD;
-        }
-        for (int i = 1000000; i >=1 ; i--) {
-            arr[i - 1] = (arr[i + 1] - arr[i]) % MOD;
-        }
-    }
-
-
     public static void main(String[] args) throws IOException {
         int n = System.in.read();
         boolean flag = n == '-';
@@ -27,18 +13,23 @@ public class Main {
             n *= 10;
             n += tmp - 48;
         }
-        if (flag) {
-            n = -n;
+        int[] arr = new int[n + 2];
+        arr[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            arr[i] = (arr[i - 2] + arr[i - 1]) % 1000000000;
         }
-        int ans = arr[n + 1000000];
-        if (ans == 0) {
-            System.out.print("0\n0");
-        } else if (ans > 0) {
-            System.out.println(1);
-            System.out.println(ans);
+        StringBuilder sb = new StringBuilder();
+        if (n == 0) {
+            sb.append("0\n");
+        } else if (flag) {
+            if ((n & 1) == 0) {
+                sb.append("-1\n");
+            } else {
+                sb.append("1\n");
+            }
         } else {
-            System.out.println(-1);
-            System.out.println(-ans);
+            sb.append("1\n");
         }
+        System.out.print(sb.append(arr[n]));
     }
 }
