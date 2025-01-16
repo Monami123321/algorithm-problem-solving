e3fe1199bf89a31c5a5ad021b880421d7d0a2d84
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-01-16 16:31:34 +0900

    [Bronze II] Title: 숫자의 개수, Time: 112 ms, Memory: 110228 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,5 +1,7 @@
 import sys, functools
 lst = [0] * 10
-for i in str(functools.reduce(lambda a, b: a * b, map(int, sys.stdin.read().rstrip().split("\n")))):
-    lst[int(i)] += 1
+n = functools.reduce(lambda a, b: a * b, map(int, sys.stdin.read().rstrip().split("\n")))
+while n:
+    lst[n % 10] += 1
+    n //= 10
 sys.stdout.write("\n".join(map(str, lst)))    
