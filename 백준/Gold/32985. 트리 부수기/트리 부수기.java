297853bb8f7fd97d578ca4d766dca595f2353cdd
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-01-18 02:04:18 +0900

    [Gold III] Title: 트리 부수기, Time: 988 ms, Memory: 107620 KB -BaekjoonHub

---
+++ 백준/Gold/32985. 트리 부수기/트리 부수기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static List<Integer>[] adjList;
+    static int[] cnt;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        adjList = new ArrayList[n];
+        cnt = new int[n];
+        for (int i = 0; i < n; i++) {
+            adjList[i] = new ArrayList<>();
+        }
+        StringTokenizer st;
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            adjList[a].add(b);
+            adjList[b].add(a);
+        }
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0});
+        cnt[0] = -1;
+        while (!q.isEmpty()) {
+            int[] now = q.poll();
+            int node = now[0];
+            int c = now[1];
+            adjList[node].forEach(e -> {
+                if (cnt[e] == 0) {
+                    cnt[e] = c + 1;
+                    q.add(new int[]{e, c + 1});
+                }
+            });
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = n - 1; i >= 1; i--) {
+            if ((n-1 - cnt[i] & 1) == 0) {
+                sb.append(0);
+            } else {
+                sb.append(1);
+            }
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}
