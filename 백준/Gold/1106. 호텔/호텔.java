42766f70f2609ec3d96e95a6dcec9ea1019de891
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-02-13 15:44:08 +0900

    [Gold IV] Title: 호텔, Time: 196 ms, Memory: 18624 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
@@ -12,24 +11,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int m = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int[] dp = new int[m + 3000];
+        int[] dp = new int[m + 101];
         final int INF = 1 << 25;
         Arrays.fill(dp, INF);
         dp[0] = 0;
-        int[][] arr = new int[n][2];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            for (int j = v; j < dp.length; j++) {
+                dp[j] = Math.min(dp[j], dp[j - v] + w);
             }
         }
-        Arrays.stream(arr).sorted(Comparator.comparingInt(a -> a[0])).forEach(e -> {
-            int w = e[0];
-            int v = e[1];
-            for (int i = v; i < dp.length; i++) {
-                dp[i] = Math.min(dp[i], dp[i - v] + w);
-            }
-        });
         System.out.println(IntStream.range(m, dp.length).map(e -> dp[e]).min().getAsInt());
         br.close();
     }
