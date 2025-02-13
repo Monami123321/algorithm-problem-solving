4d643d19958172fe8fc8acdba73329d1ba71faef
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-02-13 14:09:02 +0900

    [Gold V] Title: 수강 과목, Time: 124 ms, Memory: 14120 KB -BaekjoonHub

---
+++ 백준/Gold/17845. 수강 과목/수강 과목.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] dp = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            for (int j = n; j >= w; j--) {
+                dp[j] = Math.max(dp[j], dp[j - w] + v);
+            }
+        }
+        System.out.println(dp[n]);
+        br.close();
+    }
+}
+
