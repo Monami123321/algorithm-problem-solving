b5d3c8dd9501da88a4c849dfd4f9969fedaa1c4b
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-02-13 21:51:02 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 400 ms, Memory: 147920 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,17 +1,7 @@
-import sys, functools
-
-def cmp(a, b):
-    if a[1] != b[1]:
-        return b[1] - a[1]
-    if len(a[0]) > len(b[0]):
-        return -1
-    if len(a[0]) < len(b[0]):
-        return 1
-    return -1 if a[0] < b[0] else 1
-
+import sys
 n, m = map(int, sys.stdin.readline().split())
 dic = {}
 for l in sys.stdin.read().rstrip().split("\n"):
     if len(l) >= m:
         dic[l] = dic.get(l, 0) + 1
-sys.stdout.write("\n".join(map(lambda x: x[0], sorted(dic.items(), key=functools.cmp_to_key(cmp)))))         
+sys.stdout.write("\n".join(map(lambda x: x[0], sorted(dic.items(), key=lambda x: (-x[1], -len(x[0]), x[0]) ))))         
