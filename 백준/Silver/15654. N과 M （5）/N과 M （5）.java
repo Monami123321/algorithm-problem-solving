97f37701344dcf96e246d294b982c65c6c364ef6
유호영 <139312374+Monami123321@users.noreply.github.com>
2025-03-15 14:36:20 +0900

    [Silver III] Title: N과 M (5), Time: 208 ms, Memory: 31016 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -5,47 +5,41 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, m;
-    static StringBuilder sb;
-
+    static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        sb = new StringBuilder();
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
         Arrays.sort(arr);
-        permutation(arr, new boolean[n], new int[m], 0, 0);
+        perm(arr, new int[m], m, 0, 0);
         System.out.print(sb);
         br.close();
     }
 
-    static void permutation(int[] arr, boolean[] visited, int[] res, int pick, int depth) {
-        if (pick == m) {
+    static void perm(int[] arr, int[] res, int m, int depth, int flag) {
+        if (depth == m) {
             for (int i = 0; i < m; i++) {
                 sb.append(res[i]).append(" ");
             }
             sb.append("\n");
             return;
         }
-        if (depth == n) {
-            return;
-        }
-        for (int i = 0; i < n; i++) {
-            if (visited[i]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            int tmp = 1 << i;
+            if ((flag & tmp) != 0) {
                 continue;
             }
-            visited[i] = true;
-            res[pick] = arr[i];
-            permutation(arr, visited, res, pick + 1, depth + 1);
-            visited[i] = false;
+            flag |= tmp;
+            res[depth] = arr[i];
+            perm(arr, res, m, depth + 1, flag);
+            flag ^= tmp;
         }
-
     }
 }
